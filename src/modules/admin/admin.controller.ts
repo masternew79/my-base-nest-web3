52a88e2e74d12ce282ff9cb039f249a1b6f317c4
masternew79 <tanhoangle1996@gmail.com>
2024-04-05 11:26:40 +0700

    Add swagger

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -16,6 +16,43 @@ import { Roles } from '../auth/roles/roles.decorator';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 
+const { DType, Response, Operation, API } = require('swagger-generator-json')
+// By default will be get method
+const documentsSearchUser = new API({
+  path: '/public/users/search',
+  operation: [
+    new Operation({
+      method: DType.get,
+      parameters: [
+        {
+          name: 'username',
+          type: DType.string,
+          place: DType.query,
+          description: "Username must be required"
+        }
+      ],
+      responses: [
+        new Response({
+          schema: [{
+            id: DType.number,
+            name: DType.string,
+            isAdmin: DType.boolean
+          }]
+        }),
+        new Response({
+          code: 302,
+          schema: [{
+            id: DType.number,
+            name: DType.string,
+            isAdmin: DType.boolean
+          }]
+        })
+      ],
+      tags: 'public/user'
+    })
+  ]
+})
+
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
