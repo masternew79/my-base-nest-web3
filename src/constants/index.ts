d1e60429c29e1e996603671dd0821909792d67cb
masternew79 <tanhoangle1996@gmail.com>
2024-04-05 09:27:01 +0700

    Delete key

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ export const STATUS_CODE = `
 \n 403: Forbidden resource, Missing "Authorization" (accessToken) in header.
 \n 499: Force to login screen. (delete jwt on local)
 `;
-export const BSC_API_KEY = 'JR5W7V5HVNCUAGR155USXXTJGI2AVPF5YR';
+export const BSC_API_KEY = '';
 export const BSC_TESTNET = 97;
 export const BSC_MAINNET = 56;
 export const DATE_OF_INCORPORATION = '1-6-2023';
